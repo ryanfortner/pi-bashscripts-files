8e22ee95e6753eb2b48cb9856171c9294ac16394
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2020-12-26 17:57:29 -0500

    Update install.sh

--- wps-office/install.sh
+++ wps-office/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/9719/wps-office_11.1.0.9719_arm64.deb -O ~/wps-office_arm64.deb
+wget -O ~/wps-office_arm64.deb https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/9719/wps-office_11.1.0.9719_arm64.deb
 
 sudo apt install --fix-missing -y ~/wps-office_arm64.deb
 
