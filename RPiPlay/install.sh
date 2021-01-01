7796b73d47a25cfb2882c7930fd30bbcfe7e3ca9
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-01 14:06:29 -0500

    Add files via upload

---
+++ RPiPlay/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+git clone https://github.com/FD-/RPiPlay.git
+cd RPiPlay
+
+sudo apt-get install cmake -y
+sudo apt-get install libavahi-compat-libdnssd-dev -y
+sudo apt-get install libplist-dev -y
+sudo apt-get install libssl-dev -y
+
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+
+wget https://github.com/chunky-milk/pi-bashscripts-files/raw/main/RPiPlay/start-rpiplay.sh
+chmod +x start-rpiplay.sh
+mv start-rpiplay.sh $HOME/.local/share/start-rpiplay.sh
+
+wget https://github.com/chunky-milk/pi-bashscripts-files/raw/main/RPiPlay/668275.png
+mv 668275.png rpiplay.png
+mv rpiplay.png $HOME/.local/share/rpiplay.png
+
+echo "[Desktop Entry]
+Name=RPiPlay
+GenericName=AirPlay server
+Comment= open-source implementation of an AirPlay mirroring server
+Exec=$HOME/.local/share/start-rpiplay.sh
+Icon=$HOME/.local/share/rpiplay.png
+Terminal=true
+StartupNotify=true
+Type=Application
+Categories=Utility;" > ~/.local/share/applications/rpiplay.desktop
