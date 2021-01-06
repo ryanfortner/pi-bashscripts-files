dd14aec007e92ed105ab2f4750ff20ba3fd025f7
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-06 12:13:50 -0500

    Fixed "file not found" error

--- google-earth/earth.sh
+++ google-earth/earth.sh
@@ -13,4 +13,4 @@ if [ ! `which chromium-browser` ]; then
 fi
 
 # Launch Earth
-chromium-browser %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 11895.95.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.125 Safari/537.36" --app=https://earth.google.com/static/wasm || error "Failed to launch Google Earth!"
+chromium-browser %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 11895.95.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.125 Safari/537.36" --app=https://earth.google.com/web/wasm || error "Failed to launch Google Earth!"
