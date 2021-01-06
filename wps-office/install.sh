b2d248435f632d2067b5feb9270e4749441cb769
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-06 11:46:48 -0500

    Delete install.sh

--- wps-office/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -O ~/wps-office_arm64.deb https://wdl1.cache.wps.cn/wps/download/ep/Linux2019/9719/wps-office_11.1.0.9719_arm64.deb
-
-sudo apt install --fix-missing -y ~/wps-office_arm64.deb
-
-rm -f ~/wps-office_arm64.deb
