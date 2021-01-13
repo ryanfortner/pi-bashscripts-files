8c309ab4b900832d80ab458b4b9dc24e8b390c0c
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-13 12:44:05 -0500

    Delete start-rpiplay.sh

--- RPiPlay/start-rpiplay.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting RPiPlay. When the screen goes black, turn on Screen Mirroring on your Apple device (iPhone, iPad, Mac, etc.) and select RPiPlay as the mirroring server. To exit, stop mirroring and press control+c."
-sleep 5
-
-/usr/local/bin/rpiplay
-
-echo "Close this window to exit."
-sleep 1h
