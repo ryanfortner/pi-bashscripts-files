294bb15bca18e37279873521f1ea86150281999f
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-01-16 22:15:14 +0200

    Update uninstall.sh

--- firefox/uninstall.sh
+++ firefox/uninstall.sh
@@ -8,3 +8,4 @@ echo "uninstalling firefox..."
 sudo apt purge firefox -y
 sudo rm /etc/apt/preferences.d/99bionic-updates
 sudo rm /etc/apt/sources.list.d/ubuntu.list
+sudo apt-key del 3B4FE6ACC0B21F32
