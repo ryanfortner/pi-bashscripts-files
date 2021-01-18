ac39f509a2edfbff24b643e223c85680fb820766
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-18 10:48:45 -0500

    Add conky install script

--- conky-rings/install.sh
+++ conky-rings/install.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-sudo apt install conky-all lua50
-cp conkyrc /home/pi/.conkyrc && cp conky_rings.lua /home/pi/.conky_rings.lua
-mkdir /home/pi/.local/share/fonts
-tar -xvf fonts.tar.gz && mv *.ttf /home/pi/.local/share/fonts
-rm .uuid
-conky -q -d -p 3
+sudo apt-get update
+sudo apt install conky-all lua50 -y
 
-clear
+mkdir install-conky && cd install-conky
+
+wget -O ~/.conkyrc https://github.com/chunky-milk/pi-bashscripts-files/raw/main/conky-rings/conkyrc
+
+wget -O ~/conky_rings.lua https://github.com/chunky-milk/pi-bashscripts-files/raw/main/conky-rings/conky_rings.lua
+
+mkdir /home/pi/.local/share/fonts
+
+wget -O ~/fonts.tar.gz https://github.com/chunky-milk/pi-bashscripts-files/raw/main/conky-rings/fonts.tar.gz
+
+tar -xvf fonts.tar.gz && mv *.ttf /home/pi/.local/share/fonts
+
+rm .uuid
+
+mkdir -p ~/.config/autostart
+echo -n '' > ~/.config/autostart/conky.desktop
+echo "[Desktop Entry]
+Name=Conky
+Type=Application
+Exec=bash -c 'sleep 5;conky -q -d -p 3'
+Terminal=false
+Comment=system monitoring tool.
+Categories=Utility;" > ~/.config/autostart/conky.desktop
+
+conky -q -d -p 3 &
+
 echo ""
 echo ""
 echo "Conky Rings has been installed. Please add to session and startup or run with the following at each restart conky -q -d -p 3"
