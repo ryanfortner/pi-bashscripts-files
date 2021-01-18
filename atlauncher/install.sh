54ea24d43da8250070000dc3ffc4bb3b9484f236
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-18 11:34:38 -0500

    Update install.sh

--- atlauncher/install.sh
+++ atlauncher/install.sh
@@ -4,8 +4,15 @@
 sudo apt-get update
 sudo apt-get install openjdk-11-jdk -y
 
+# Make ATLauncher directory
 mkdir ~/ATLauncher
 cd ~/ATLauncher
 
+# Get ATLauncher jarfile
 wget -O ATLauncher.jar https://atlauncher.com/download/jar
 
+# get lwjgl libraries
+wget https://github.com/mikehooper/Minecraft/raw/main/lwjgl3arm32.tar.gz
+wget https://github.com/mikehooper/Minecraft/raw/main/lwjgl2arm32.tar.gz
+tar -zxf lwjgl3arm32.tar.gz -C ~/lwjgl3arm32
+tar -zxf lwjgl2arm32.tar.gz -C ~/lwjgl2arm32
