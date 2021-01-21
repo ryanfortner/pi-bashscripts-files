9ce72a18a54f9cb694f27a0d11b5b76200e4f37c
chunkymilk0309 <71036629+chunky-milk@users.noreply.github.com>
2021-01-21 13:02:06 -0500

    Create install.sh

---
+++ alacritty/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget -O ~/alacritty.zip https://github.com/chunky-milk/pi-bashscripts-files/raw/main/alacritty/alacritty-0.6.0-arm32.zip
+cd ~/
+unzip alacritty.zip
+
+echo "[Desktop Entry]
+Name=Alacritty
+Exec=$HOME/alacritty/alacritty
+Path=$HOME/alacritty/
+Icon=$HOME/alacritty/icon.png
+Type=Application
+Comment=Alacritty is the fastest terminal emulator in existence. Using the GPU for rendering enables optimizations that simply aren't possible without it.
+Categories=ConsoleOnly;Utility;System; > ~/.local/share/applications/alacritty.desktop
