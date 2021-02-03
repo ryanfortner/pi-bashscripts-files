4a402d0c4f2e9b57800e32f78366480bf60a58e0
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-02-03 23:35:26 +0200

    Create uninstall.sh

---
+++ bleachbit/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##########################################################
+# script by Itai-Nelken - https://github.com/Itai-Nelken #
+##########################################################
+
+echo "uninstalling bleachbit..."
+
+sudo apt purge bleachbit -y
