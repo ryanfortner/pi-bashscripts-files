6e139c39e65bbae2375a31284aaa2175093886c6
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-02-04 15:43:01 +0200

    Create uninstall.sh

---
+++ balenaetcher/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##########################################################
+# script by Itai-Nelken - https://github.com/Itai-Nelken #
+##########################################################
+
+echo "uninstalling BalenaEtcher..."
+sudo apt purge -y balena-etcher-electron
+echo "DONE!"
