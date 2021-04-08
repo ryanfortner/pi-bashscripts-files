42300ad036a1a85c798135f48c405cf130ff7529
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-08 16:30:02 +0100

    Got bored and done more!

--- checkinstall/install.sh
+++ checkinstall/install.sh
@@ -5,8 +5,8 @@
 # debs packaged by Itai-Nelken - ^                       #
 ##########################################################
 
-echo "installing checkinstall..."
-echo "getting system architecture..."
+echo "Installing checkinstall..."
+echo "Getting system architecture..."
 #determine if host system is 64 bit arm64 or 32 bit armhf
 if [ ! -z "$(file "$(readlink -f "/sbin/init")" | grep 64)" ];then
   ARCH=64
@@ -18,17 +18,17 @@ else
 fi
 
 if [[ "$ARCH" == 32 ]]; then
-  echo "downloading armhf checkinstall..."
+  echo "Downloading Armhf checkinstall..."
   wget https://archive.org/download/macos_921_qemu_rpi/checkinstall_20210123-1_armhf.deb -O ~/checkinstall_armhf.deb
   cd $HOME
-  echo "installing armhf checkinstall..."
+  echo "Installing Armhf checkinstall..."
   sudo apt -f -y install ./checkinstall_armhf.deb
   rm checkinstall_armhf.deb
 elif [[ "$ARCH" == 64 ]]; then
-  echo "downloading arm64 checkinstall..."
+  echo "Downloading ARM64 checkinstall..."
   wget https://archive.org/download/macos_921_qemu_rpi/checkinstall_20210126-1_arm64.deb -O ~/checkinstall_arm64.deb
   cd $HOME
-  echo "installing arm64 checkinstall..."
+  echo "Installing ARM64 checkinstall..."
   sudo apt -f -y install ./checkinstall_arm64.deb
   rm checkinstall_arm64.deb
 fi
--- checkinstall/old-readme.md
+++
@@ -1,7 +0,0 @@
-https://github.com/opencv/opencv/issues/8897
-```
-sudo apt-get update && sudo apt-get install checkinstall -y
-git clone https://github.com/giuliomoro/checkinstall
-cd checkinstall
-sudo make install
-```
