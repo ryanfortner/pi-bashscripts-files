c70a979e9d35359dab42bb875f98f534b7c2f48d
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-09 18:21:27 +0100

    Adding new gh

--- github-cli/install.sh
+++ github-cli/install.sh
@@ -10,13 +10,13 @@ echo "Installing Github-CLI."
 if [ ! -z "$(file "$(readlink -f "/sbin/init")" | grep 64)" ];then
   #install 64bit gh
   echo "Installing Arm64 version."
-  wget -O ~/gh-cli_arm64.deb https://github.com/cli/cli/releases/download/v1.5.0/gh_1.5.0_linux_arm64.deb
+  wget -O ~/gh-cli_arm64.deb https://github.com/cli/cli/releases/download/v1.8.1/gh_1.8.1_linux_arm64.deb
   sudo apt -f -y install ~/gh-cli_arm64.deb
   rm ~/gh-cli_arm64.deb
 elif [ ! -z "$(file "$(readlink -f "/sbin/init")" | grep 32)" ];then
   #install 32bit version
   echo "Installing Armhf version."
-  wget -O ~/gh-cli_armhf.deb https://github.com/cli/cli/releases/download/v1.5.0/gh_1.5.0_linux_armv6.deb
+  wget -O ~/gh-cli_armhf.deb https://github.com/cli/cli/releases/download/v1.8.1/gh_1.8.1_linux_armv6.deb
   sudo apt -f -y install ~/gh-cli_armhf.deb
   rm ~/gh-cli_armhf.deb
 else
